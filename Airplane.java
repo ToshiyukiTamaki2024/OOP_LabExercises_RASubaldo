6345d4ced22e2bb023c4df69195f2c78a28ccc51
RA"L"PS <remarsubaldo@gmail.com>
2025-09-25 19:59:35 +0800

    Delete Airplane.java

--- Airplane.java
+++
@@ -1,5 +0,0 @@
-public class Airplane extends AirTransport {
-    public Airplane(String name, int capacity, double speed, int maxAltitude) {
-        super(name, capacity, speed, maxAltitude);
-    }
-}
