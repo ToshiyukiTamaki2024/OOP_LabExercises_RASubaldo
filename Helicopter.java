5f75752624f5b9e5122d3da8e82624e884b380a6
RA"L"PS <remarsubaldo@gmail.com>
2025-09-25 20:09:04 +0800

    Delete Helicopter.java

--- Helicopter.java
+++
@@ -1,5 +0,0 @@
-public class Helicopter extends AirTransport {
-    public Helicopter(String name, int capacity, double speed, int maxAltitude) {
-        super(name, capacity, speed, maxAltitude);
-    }
-}
