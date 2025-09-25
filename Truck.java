f051767c6bf2394df11e4cbf4b3f7129eba5e47a
RA"L"PS <remarsubaldo@gmail.com>
2025-09-25 20:43:14 +0800

    Delete Truck.java

--- Truck.java
+++
@@ -1,5 +0,0 @@
-public class Truck extends LandTransport {
-    public Truck(String name, int capacity, double speed) {
-        super(name, capacity, speed, 6); // Trucks commonly have 6 wheels
-    }
-}
